96e7c0050967b78398f2e3f31de60ee4e582bb36
Ivo <81419894+gewooniv@users.noreply.github.com>
2024-03-31 11:44:43 +0200

    recreate modal.jsx

---
+++ src/components/Modal.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+export default function Modal({ open, children, onClose }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    } else {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog ref={dialog} onClose={onClose}>
+      {children}
+    </dialog>,
+    document.getElementById("root"),
+  );
+}
