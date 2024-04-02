7515be9b8ae698979bcbab7dd896cd47d0211486
Ivo <81419894+gewooniv@users.noreply.github.com>
2024-04-02 09:33:24 +0000

    added answer selection

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,24 @@
-import { useQuestions } from "./QuizProvider.jsx";
 import { useState } from "react";
 import Modal from "./Modal.jsx";
+import QUESTIONS from ".././data/questions.js";
 
 export default function Quiz() {
-  const questions = useQuestions();
-  const question = questions.find((question) => question.answer === undefined);
   const [modalOpen, setModalOpen] = useState(false);
+  const [userAnswers, setUserAnswers] = useState([]);
 
-  if (!question) {
-    setModalOpen(true);
+  let activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(answer) {
+    setUserAnswers((prevState) => {
+      return [...prevState, answer];
+    });
+    console.log(userAnswers);
+    console.log(QUESTIONS.length);
+
+    if (activeQuestionIndex === QUESTIONS.length - 2) {
+      setModalOpen(true);
+      activeQuestionIndex = 0; // temp solution
+    }
   }
 
   function handleModalClose() {
@@ -17,20 +27,25 @@ export default function Quiz() {
 
   return (
     <section>
-      <Modal open={modalOpen} onClose={handleModalClose}>
-        {modalOpen &&
-          "Lorem ipsum betekent niets, het is een verzameling Latijnse woorden die een tekst vormen van nep vullen, willekeurig, plaatsaanduiding."}
-      </Modal>
+      {modalOpen && (
+        <Modal open={modalOpen} onClose={handleModalClose}>
+          "Lorem ipsum betekent niets, het is een verzameling Latijnse woorden
+          die een tekst vormen van nep vullen, willekeurig, plaatsaanduiding."
+        </Modal>
+      )}
       <div id="quiz">
         <div id="question">
           <progress />
-          <h2>{question.text}</h2>
+          <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         </div>
         <ul id="answers">
           <li className="answer">
-            {question.answers.map((answer) => {
+            {QUESTIONS[activeQuestionIndex].answers.map((answer) => {
               return (
-                <button key={Math.random()} onClick={() => setModalOpen(true)}>
+                <button
+                  key={answer}
+                  onClick={(answer) => handleSelectAnswer(answer)}
+                >
                   {answer}
                 </button>
               );
--- src/components/QuizProvider.jsx
+++ src/components/QuizProvider.jsx
@@ -1,20 +1,13 @@
-import QUESTIONS from ".././data/questions.js";
 import { createContext, useContext, useState } from "react";
 
 const QuizContext = createContext();
 
 export default function QuizProvider({ children }) {
-  const [questions, setQuestions] = useState(QUESTIONS);
-
-  questions.map((question) => {
-    return { ...question, answer: undefined };
-  });
-
   return (
-    <QuizContext.Provider value={questions}>{children}</QuizContext.Provider>
+    <QuizContext.Provider value={undefined}>{children}</QuizContext.Provider>
   );
 }
 
-export function useQuestions() {
-  return useContext(QuizContext);
-}
+// export function useAnswers() {
+//   return useContext(QuizContext);
+// }
